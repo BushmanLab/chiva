b2a9dc8eb3a4cd522685808934d349bb11d827e7
kevin <kevinmcc@upenn.edu>
2020-12-24 00:00:01 -0500

    automated commit

--- sampleStats.R
+++ sampleStats.R
@@ -6,7 +6,7 @@ analysisDir <- args[2]
 
 sampleInfo <- read.table(sampleInfoFile, sep = ",", header = TRUE, stringsAsFactors = FALSE) %>%
   rowwise() %>%
-  mutate(sample=str_split(str_split(sampleName,"-")[[1]][1],"_")[[1]][1]) %>%
+  mutate(sample=stringr::str_split(stringr::str_split(sampleName,"-")[[1]][1],"_")[[1]][1]) %>%
   ungroup() %>% as.data.frame()
 
 summary <- do.call(rbind, lapply(unique(sampleInfo$sample), function(sample) {
@@ -26,34 +26,6 @@ summary <- do.call(rbind, lapply(unique(sampleInfo$sample), function(sample) {
                     U5R2LTRreads=U5R2LTRreads))
 }))
 
-write.table(,
+write.table(summary,
             paste0(args[1],".summary.csv"), sep = ",", row.names = FALSE, quote = FALSE)
-
-subsample_dir <- function(dir) {
-  dataDir <- paste0(dir, "/analysis_data/")
-  subsampleDir <- paste0(dir, "/subsample/")
-  system(paste0("mkdir ", subsampleDir))
-  fastqDir <- paste0(dir, "/subsample/fastq.gz/")
-  system(paste0("mkdir ", fastqDir))
-  csvDir <- paste0(dir, "/subsample/csv/")
-  system(paste0("mkdir ", csvDir))
-  pdfDir <- paste0(dir, "/subsample/pdf/")
-  system(paste0("mkdir ", pdfDir))
-  
-  sampleInfoFile <- paste0(dir, ".sampleInfo.csv")
-  if(!file.exists(sampleInfoFile)) sampleInfoFile <- gsub("_",".",sampleInfoFile)
-  
-  samples <- read.table(sampleInfoFile, sep = ",", header = TRUE, stringsAsFactors = FALSE) %>%
-    filter(substr(sampleName, 1, 1) == "G") %>%
-    mutate(R1=paste0(dataDir,sampleName,".R1.fastq.gz"),
-           R2=paste0(dataDir,sampleName,".R2.fastq.gz"),
-           GTSP=substr(sampleName, 1, 8))
-  
-  lapply(unique(samples$GTSP), function(GTSP) {
-    print(paste0("Processing ", GTSP))
-    subsample(dataDir, GTSP, fastqDir)
-  })
-}
-
-
 
